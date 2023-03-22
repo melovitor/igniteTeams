70e697d487fc4fdc0a9381ef1f6c178f7532fc0e
melovitor <vitormelobatis@gmail.com>
2023-03-22 14:07:49 -0300

    WIP - efinindo o tema da aplicação.

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,10 @@
-import { Text, View } from 'react-native';
 import { Container } from './styles';
 
 
 export function Groups() {
   return (
     <Container>
-      <Text>Groups</Text>
+
     </Container>
     
   );
