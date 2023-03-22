900fed257b3e0eca279183b198cf1af0bd3f4567
melovitor <vitormelobatis@gmail.com>
2023-03-22 15:00:51 -0300

    WIP - Componente de Loading.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import { Groups } from "@screens/Groups";
 import { ThemeProvider } from "styled-components";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { Loading } from "@components/Loading";
 import theme from "./src/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+
+  
+
+
   return (
-    <ThemeProvider theme={theme}>
-      <Groups/> 
+    <ThemeProvider theme={theme}> 
+      {fontsLoaded ? <Groups/> : <Loading/> }
     </ThemeProvider>
   );
 }
