97429d6742d329d2b6ffea3e41512e780a9f7236
melovitor <vitormelobatis@gmail.com>
2023-03-21 21:26:31 -0300

    WIP - Path Mapping.

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export function Groups() {
+  return (
+    <View >
+      <Text>Groups</Text>
+    </View>
+  );
+}
