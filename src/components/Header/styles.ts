f2417026c3cb1097f6faf38e0d066f21a0ac9d92
melovitor <vitormelobatis@gmail.com>
2023-03-22 20:40:27 -0300

    WIP - Adicionando icone.

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,6 @@
 import styled from "styled-components/native";
+import { CaretLeft} from "phosphor-react-native"
+
 
 export const Container = styled.View`
     width: 100%;
@@ -10,4 +12,13 @@ export const Container = styled.View`
 export const Logo = styled.Image`
     width: 46px;
     height: 55px;
+`;
+
+export const BackButton = styled.TouchableOpacity`
+    flex: 1;
 `
+
+export const BackIcon = styled(CaretLeft).attrs(({theme}) => ({
+    size: 32,
+    color: theme.COLORS.WHITE
+}))``;
