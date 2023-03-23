c087bb288d25b2b4ed0472891e0f8aa02d275337
melovitor <vitormelobatis@gmail.com>
2023-03-23 15:00:22 -0300

    WIP - Criando lista de grupos.

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
@@ -5,11 +7,18 @@ import { GroupCard } from '@components/GroupCard';
 
 
 export function Groups() {
+  const [groups, setGroups] = useState(['R6 Team', 'Fortnite Team']);
+
   return (
     <Container>
       <Header />
-      <Highlight title='Turmas' subtitle='Jogue com a sua turma!'/>
-      <GroupCard title='hello'/>
+      <Highlight title='Teams' subtitle='Jogue com a sua turma!'/>
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => <GroupCard title={item}/>}
+      />
     </Container>
     
   );
