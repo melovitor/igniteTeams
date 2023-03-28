cc8dad37e6362b0cb506ae7d038573044319621e
melovitor <vitormelobatis@gmail.com>
2023-03-27 22:42:51 -0300

    WIP - Iniciando a interface de NewGroup

---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@components/Button'
+import { Header } from '@components/Header'
+import { Highlight } from '@components/Highlight'
+import {Container, Content, Icon} from './styles'
+
+export function NewGroup(){
+    return (
+        <Container>
+            <Header showBackButton={true}/>
+            <Content>
+                <Icon/>
+                <Highlight title='Nova turma' subtitle='Crie uma nova turma para adicionar pessoas!'/>
+                <Button title='Criar nova turma'/>
+            </Content>
+        </Container>
+    )
+}
