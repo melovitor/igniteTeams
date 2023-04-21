b514b806b062c77630acca0efbef46936d66ab41
melovitor <vitormelobatista@hotmail.com>
2023-04-21 15:39:39 -0300

    WIP - Finalizando o Input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Button } from '@components/Button'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import {Container, Content, Icon} from './styles'
+import { Input } from '@components/Input'
 
 export function NewGroup(){
     return (
@@ -10,7 +11,8 @@ export function NewGroup(){
             <Content>
                 <Icon/>
                 <Highlight title='Nova turma' subtitle='Crie uma nova turma para adicionar pessoas!'/>
-                <Button title='Criar nova turma'/>
+                <Input placeholder='Nome da turma' />
+                <Button title='Criar nova turma' style={{marginTop: 20}}/>
             </Content>
         </Container>
     )
