107a0b5bf804c8dd93ab3c69319151a81ebe9197
melovitor <vitormelobatista@hotmail.com>
2023-05-05 13:20:02 -0300

    WIP - Criando componente de ButtonIcon.

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,7 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
+import { ButtonIcon } from "@components/ButtonIcon";
 
 export function Players(){
     return(
@@ -10,6 +11,8 @@ export function Players(){
                 title="Nome da Turma"
                 subtitle="Adicione a galera e separe os times!"            
             />
+            <ButtonIcon/>
+            
         </Container>
 
     )
