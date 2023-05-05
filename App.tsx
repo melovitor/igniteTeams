0f0e25793c19d87167c47eb01fcc5d71ae748d6f
melovitor <vitormelobatista@hotmail.com>
2023-05-05 12:15:38 -0300

    WIP - Utilizando o CSS Helper.

--- App.tsx
+++ App.tsx
@@ -19,7 +19,8 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      {fontsLoaded ? <NewGroup/> : <Loading/> }
+      {/* {fontsLoaded ? <NewGroup/> : <Loading/> } */}
+      {fontsLoaded ? <Groups/> : <Loading/> }
     </ThemeProvider>
   );
 }
