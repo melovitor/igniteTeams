ca65807e9593ad80aa58c0af7724ed1f881c1a9c
melovitor <vitormelobatista@hotmail.com>
2023-05-08 15:46:23 -0300

    WIP - Finalizando a lista de players.

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -7,11 +7,13 @@ import { Filter } from "@components/Filter";
 import { FlatList } from "react-native";
 import { useState } from "react";
 import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Players(){
 
     const [team, setTeam] = useState('time a')
-    const [players, setPlayers] = useState(['Vitor', 'melo'])
+    const [players, setPlayers] = useState([])
 
     return(
         <Container>
@@ -40,7 +42,8 @@ export function Players(){
                             onPress={() => setTeam(item)}                        
                         />
                     )}
-                    horizontal            
+                    horizontal  
+                    showsHorizontalScrollIndicator={false}       
                 />
                 <NumbersOfPlayers>{players.length}</NumbersOfPlayers>
             </HeaderList>
@@ -53,7 +56,14 @@ export function Players(){
                         onRemove={() => {}}
                     />
                 }
+                ListEmptyComponent= {() => <ListEmpty title='' subtitle='Não há pessoas nesse time.'/>}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={[
+                    {paddingBottom: 100},
+                    players.length === 0 && {flex: 1}
+                ]}
             />
+            <Button title="Remover turma" type="RED"/>
 
             
         </Container>
