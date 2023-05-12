4f2a983d3461263c4feffd6f498d314f8b83e55c
melovitor <vitormelobatista@hotmail.com>
2023-05-12 15:49:01 -0300

    WIP - Obtendo os grupos.

--- src/storage/group/groupCrate.ts
+++ src/storage/group/groupCrate.ts
@@ -1,9 +1,12 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GROUP_COLECTION } from "@storage/storageConfig";
+import { geyAllGroups } from "./getAllGoups";
 
 export async function groupCrate(newGroup: string) {
     try {
-        await AsyncStorage.setItem(GROUP_COLECTION, newGroup)
+        const storedGroups = await geyAllGroups()
+        const storage = JSON.stringify([...storedGroups, newGroup])
+        await AsyncStorage.setItem(GROUP_COLECTION, storage)
     } catch (e) {
         throw e
     }
